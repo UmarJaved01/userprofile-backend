722c86fc19b6d309536fbb5b7da0ed94b1ac2f5a
UmarJaved01 <umar@shahtez.com>
2025-03-16 20:33:18 +0500

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ const generateAccessToken = (user) => {
 };
 
 const generateRefreshToken = (user) => {
-  return jwt.sign({ user: { id: user._id } }, process.env.REFRESH_SECRET, { expiresIn: '7d' });
+  return jwt.sign({ user: { id: user._id } }, process.env.REFRESH_SECRET, { expiresIn: '2m' });
 };
 
 // Determine cookie settings based on environment
